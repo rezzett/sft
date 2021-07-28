fbab7e864a4b993ac52394ad805c77a751190841
rezzett <rezzett@mail.ru>
2021-07-29 02:34:42 +0600

    fastfix

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ mod game;
 mod game_obj;
 mod player;
 
+// TODO resource manger
+// TODO bullet struct and logic
+// TODO objects kind (meteor fuel ammo health ...?)
+// TODO gui (hp bar, fuel bar ammo bar disnace to finish???)
+// TODO main menu (replay quit setting)
+// TODO audio for shoot and colision + bg sound
+// TODO score (max distance?) and save score to file
+
 fn main() {
     let tex = Texture::from_file("ship.png").unwrap();
     let go_tex = Texture::from_file("go1.png").unwrap();
